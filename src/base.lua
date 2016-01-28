ca2d4f17583ed7c34f0de8d4c2670cb830340f1d
Ratan Varghese <ratan.varghese@gmail.com>
2016-01-27 20:08:04 -0500

    Implemented and tested remove_v and contrary (previously called mirrored)

--- src/base.lua
+++ src/base.lua
@@ -1,2 +1,68 @@
 base = {}
-function base.mirrored() return {},{} end
+
+function base.remove_v(t, targ_v)
+    local targ_k = {}
+    for k,v in pairs(t) do
+        if v == targ_v then
+            targ_k[k] = true
+        end
+    end
+    
+    count = 0
+    for k in pairs(targ_k) do
+        t[k] = nil
+        count = count + 1
+    end
+    return count
+end
+
+--[[
+CONTRARY TABLES
+
+The key-value pairs of one are the value-key pairs of the other.
+They both update at once, one with the opposite key-value to the other.
+The metatables of contrary metatables cannot be accessed or changed.
+Contrary tables cannot be iterated over using pairs, but pairs-equivalent
+iterators are returned as the last two arguments.
+Iterable key-values in init will be transferred to the forward table,
+and transferred to the reverse table as value-keys.
+--]]
+
+function base.contrary(init)
+    local function contrary_mt(fmt, rmt)
+        fmt.__index = fmt.storage
+        fmt.__newindex = function(t, k, v)
+            if fmt.storage[k] then
+                --removing old values in reverse table
+                base.remove_v(rmt.storage, k)
+            end
+            fmt.storage[k] = v
+            rmt.storage[v] = k
+        end
+        fmt.__metatable = false
+    end
+
+    local function contrary_pairs(mt)
+        return function()
+            return next, mt.storage, nil
+        end
+    end
+
+    local forward = {}
+    local reverse = {}
+
+    local fmt = {storage = {}}
+    local rmt = {storage = {}}
+    contrary_mt(fmt, rmt)
+    contrary_mt(rmt, fmt)
+    setmetatable(forward, fmt)
+    setmetatable(reverse, rmt)
+
+    if init then
+        for k,v in pairs(init) do
+            forward[k] = v
+        end
+    end
+
+    return forward, reverse, contrary_pairs(fmt), contrary_pairs(rmt)
+end
--- test/_base.lua
+++ test/_base.lua
@@ -2,41 +2,88 @@ require("_common")
 require("base")
 
 results = {}
-
-ik = "Initial key"
-iv = "Initial value"
-ft, rt = base.mirrored{[ik]=iv}
-results["mirrored: initial forward"] = ft[ik] == iv
-results["mirrored: initial reverse"] = rt[iv] == ik
-fk = "Forward key"
-fv = "Forward value"
-ft[fk] = fv
-results["mirrored: newindex forward"] = ft[fk] == fv
-results["mirrored: newindex reverse"] = rt[fv] == fk
-nv = "New value"
-ft[fk] = nv
-results["mirrored: overwrite forward"] = ft[fk] == nv
-results["mirrored: overwrite reverse"] = rt[nv] == fk
-rk = "Reverse key"
-rv = "Reverse value"
-rt[rk] = rv
-results["mirrored: newindex forward (opposite)"] = ft[rv] == rk
-results["mirrored: newindex reverse (opposite)"] = rt[rk] == rv
-anv = "Another new value"
-rt[rk] = anv
-results["mirrored: overwrite forward (opposite)"] = ft[anv] == rk
-results["mirrored: overwrite reverse (opposite)"] = rt[rk] == anv
-results["mirrored: full key-value check forward"] = true
-for k,v in pairs(ft) do
-    if rt[v] ~= k then
-        results["mirrored: full key-value check forward"] = false
+--REMOVE_V
+set_t = {"Just", "called", "to", "say", "that", "it's", "good", "to be"}
+set_f = {"I", "can", "ride", "my", "bike", "with", "no", "handlebars" }
+target = {}
+for k,v in pairs(set_t) do
+    target[v] = "keep"
+end
+for k,v in pairs(set_f) do
+    target[v] = "remove"
+end
+base.remove_v(target, "remove")
+results["remove_v: permitting"] = true
+for k,v in pairs(set_t) do
+    if target[v] ~= "keep" then
+        results["remove_v: permitting"] = false
         break
     end
 end
-results["mirrored: full key-value check reverse"] = true
-for k,v in pairs(rt) do
+results["remove_v: limiting"] = true
+for k,v in pairs(set_f) do
+    if target[v] then
+        results["remove_v: limiting"] = false
+    end
+end
+
+--CONTRARY
+ik = "Initial key"
+iv = "Initial value"
+ft, rt, fpairs, rpairs = base.contrary{[ik]=iv}
+expected_k = {[ik] = iv}
+results["contrary: initial forward"] = ft[ik] == iv
+results["contrary: initial reverse"] = rt[iv] == ik
+fk = "Forward key"
+fv = "Forward value"
+ft[fk] = fv
+expected_k[fk] = fv
+results["contrary: newindex forward"] = ft[fk] == fv
+results["contrary: newindex reverse"] = rt[fv] == fk
+nv = "New value"
+ft[fk] = nv
+expected_k[fk] = nv
+results["contrary: overwrite forward"] = ft[fk] == nv
+results["contrary: overwrite reverse"] = rt[nv] == fk
+rk = "Reverse key"
+rv = "Reverse value"
+rt[rk] = rv
+expected_k[rv] = rk
+results["contrary: newindex forward (opposite)"] = ft[rv] == rk
+results["contrary: newindex reverse (opposite)"] = rt[rk] == rv
+anv = "Another new value"
+rt[rk] = anv
+expected_k[rv] = nil
+expected_k[anv] = rk
+results["contrary: overwrite forward (opposite)"] = ft[anv] == rk
+results["contrary: overwrite reverse (opposite)"] = rt[rk] == anv
+results["contrary: full key-value check forward"] = true
+results["contrary: fpairs keys limiting"] = true
+actual_k = {}
+for k,v in fpairs() do
+    if rt[v] ~= k then
+        results["contrary: full key-value check forward"] = false
+        print(k, v, rt[v])
+    end
+    if not expected_k[k] then
+        results["contrary: fpairs keys limiting"] = false
+    end
+    actual_k[k] = v 
+end
+results["contrary: fpairs keys permitting"] = true
+results["contrary: fpairs expected key-values"] = true
+for k,v in pairs(expected_k) do
+    if not actual_k[k] then
+        results["contrary: fpairs keys permitting"] = false
+    end
+    if actual_k[k] ~= v then
+        results["contrary: fpairs expected key-values"] = false
+    end
+end
+results["contrary: full key-value check reverse"] = true
+for k,v in rpairs() do
     if ft[v] ~= k then
-        results["mirrored: full key-value check forward"] = false
+        results["contrary: full key-value check forward"] = false
         break
     end
 end
