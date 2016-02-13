da81c714f959f1112d90043ba07308dec503dd34
Ratan Varghese <ratan.varghese@gmail.com>
2016-02-13 18:33:52 -0500

    Quieter test results

--- test/_common.lua
+++ test/_common.lua
@@ -1,8 +1,16 @@
 function print_results(tests, module_name)
-    print("Testing " .. module_name)
+    local printed_title = false
     assert(debug, "Attempt to run without test flag")
     for k,v in pairs(tests) do
-        print(string.format("\t%-44s%+6s", k, tostring(v)))
+        if not v then
+            if not printed_title then
+                print("Failure(s) in " .. module_name)
+                printed_title = true
+            end
+            print(k)
+        end
     end
-    print("")
+    if printed_title then
+        print("")
+    end
 end
